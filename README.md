1f28a9a2a0453afcce0db5a964ab410ad110958b
medemirhan <mahmutesat@gmail.com>
2023-01-05 00:26:24 +0300

    modifications for using local SQL server

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # TodoApi
-Implementation of tutorials below:
+Web API with ASP.NET Core
 
+References:
 Tutorial: Create a web API with ASP.NET Core:
 https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-7.0&tabs=visual-studio
 
 Tutorial: Call an ASP.NET Core web API with JavaScript:
 https://learn.microsoft.com/en-us/aspnet/core/tutorials/web-api-javascript?view=aspnetcore-7.0
+
+Build a RESTful Web API with ASP.NET Core 6:
+https://medium.com/net-core/build-a-restful-web-api-with-asp-net-core-6-30747197e229
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -6,10 +6,12 @@ using TodoApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddDbContext<TodoContext>(opt =>
+//    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
